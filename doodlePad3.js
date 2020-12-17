26d63b12aa2b18955770cec952e4ed083d4e3daf
Justin Foster <jfoster91@gmail.com>
2020-12-17 11:42:28 -0700

    12/17

--- doodlePad3.js
+++ doodlePad3.js
@@ -1,12 +1,32 @@
+function anagramCompare(wordA, wordB) {
+  let countA = {};
+  let countB = {};
 
-// function order2(word){
-//   let key = {};
-//   key[word.replace(/[a-z]/gi, '')] = word
-//   return key;
-// }
+  wordA.split('').map(char => (countA[char]) ? countA[char]++ : countA[char] = 1);
+  wordB.split('').map(char => (countB[char]) ? countB[char]++ : countB[char] = 1);
 
-function order2(word){
-  return {position: word.replace(/[a-z]/gi, ''), word: word};
+  return (isEqual(countA, countB));
 }
 
-console.log(order2('T4est'))
+function anagrams(word, words) {
+  return words.filter(wordComp => anagramCompare(word, wordComp))
+}
+
+function isEqual(obj1, obj2) {
+  let props1 = Object.getOwnPropertyNames(obj1);
+  let props2 = Object.getOwnPropertyNames(obj2);
+  
+  if (props1.length != props2.length) {
+    return false;
+  }  for (let i = 0; i < props1.length; i++) {
+    let prop = props1[i];
+    
+    if (obj1[prop] !== obj2[prop]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
