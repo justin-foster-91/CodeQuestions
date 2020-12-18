ef2e657f6f28e50bc201000548779460a5aadbf4
Justin Foster <jfoster91@gmail.com>
2020-12-18 00:09:58 -0700

    12/17

---
+++ objectDrills.js
@@ -0,0 +1,9 @@
+function Person(first, last, age, eye) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eye;
+}
+
+var myFather = new Person("John", "Doe", 50, "blue");
+var myMother = new Person("Sally", "Rally", 48, "green");
